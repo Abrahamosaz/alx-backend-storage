3c479b08ac6a3577a00416c71405b38ff7268cef
abraham <abrahamosazee3@gmail.com>
2023-08-12 22:48:14 +0100

    fix task 1 error

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
--- script that creat a table user with the below fields
--- id, name, email, country
+-- Script that creates a table users following these requirements:
+-- id, email, name, country(enumeration of US, CO and TN)
 CREATE TABLE IF NOT EXISTS users(
-id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-email VARCHAR(255) NOT NULL UNIQUE,
-name VARCHAR(255),
-country ENUM('US',  'CO', 'TI') DEFAULT 'US' NOT NULL
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 );

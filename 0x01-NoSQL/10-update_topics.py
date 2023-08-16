4d497185fc3c40403fd29c7c4a67ef0bd810099f
abraham <abrahamosazee3@gmail.com>
2023-08-16 21:15:50 +0100

    fix error

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,6 +4,7 @@ function that change all topics of a school document base
 on the name argument
 """
 
+
 def update_topics(mongo_collection, name, topics):
     """
     change all topics base on the name arg
